7a03d79bc1ceb172edd05cb5823ad845b49986cb
Dainis Abols <dainisa@lu.lv>
2021-08-13 16:23:49 +0300

    Step 9

--- app/src/main/java/com/example/myfirstapp/FirstFragment.kt
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstapp.databinding.FragmentFirstBinding
@@ -45,10 +47,31 @@ class FirstFragment : Fragment() {
             // show the Toast
             myToast.show()
         }
+
+         // Increase counter
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun countMe(view: View) {
+        // Get the text view
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count = countString.toInt()
+        count++
+
+        // Display the new value in the text view.
+        showCountTextView.text = count.toString()
     }
 }
